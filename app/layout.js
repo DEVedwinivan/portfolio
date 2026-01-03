2bbdd2ccb27eb51909edd9fcbd69e417680f1768
Edwin Rodriguez <edwinrodriguez2603@icloud.com>
2026-01-02 23:34:13 -0600

    SEO correction

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,28 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 
 export const metadata = {
-  title: "Edwin Rodriguez - Web Developer",
-  description: "Desarrollador Web especializado en PHP y LARAVEL | Portfolio de Edwin Rodriguez",
-}
+    title:{
+        default: 'Edwin Rodríguez - Desarrollador Web y BackEnd',
+        template: '%s - Edwin Rodríguez',
+    },
+    description: 'Portafolio de Edwin Rodríguez, desarrollador web y backend especializado en LARAVEL, PHP y aplicaciones modernas',
+    openGraph: {
+        title: 'Edwin Rodríguez - Desarrollador Web y BackEnd',
+        description: 'Portafolio profesional de Edwin Rodríguez, mostrando proyectos y habilidades en desarrollo web y backend.',
+        url: 'https://portfolio-ten-henna-pvkh2ge8el.vercel.app/',
+        siteName: 'Portafolio Edwin Rodríguez',
+        images: [
+            {
+                url: '/og-image.png',
+                width: 1200,
+                height: 630,
+                alt: 'Portafolio de Edwin Rodríguez',
+            },
+        ],
+        locale: 'es_MX',
+        type: 'website',
+    },
+};
 
 export default function RootLayout({ children }) {
   return (
